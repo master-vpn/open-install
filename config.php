44b4d3396c90941e3a8f7aee719443c825497476
master-vpn <117630796+master-vpn@users.noreply.github.com>
2022-11-07 15:09:17 +0700

    Update config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+{"Version":"1.1.0",
+
+"Url":"https://github.com/droperos/atcglub/raw/main/config.zip" ,
+
+"Changelog":"
+🆕อัพเดทตอนนี้ กด ตกลง👇
+",
+
+"latestVersion": "1.0.0",
+
+"latestVersionCode": "1",
+
+"url": "",
+
+ "releaseNotes": [
+
+ "-Demo"
+
+  ]
+
+}
