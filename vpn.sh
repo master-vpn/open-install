d23233d3d82dc7f8bb0c84ec9e1c37848cd29d8c
master-vpn <117630796+master-vpn@users.noreply.github.com>
2023-03-19 15:47:12 +0700

    Update and rename README.md to vpn.sh

---
+++ vpn.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# สคริป​โดย​ master-vpn
+==================================================
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=$(wget -qO- icanhazip.com);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+ANU=$(ip -o $ANU -4 route show to default | awk '{print $5}');
+
+# Install OpenVPN dan Easy-RSA
+apt install openvpn easy-rsa unzip -y
+apt install openssl iptables iptables-persistent -y
+mkdir -p /etc/openvpn/server/easy-rsa/
+cd /etc/openvpn/
+wget https://github.com/master-vpn/open-install/raw/main/vpn.zip
+unzip vpn.zip
+rm -f vpn.zip
+chown -R root:root /etc/openvpn/server/easy-rsa/
+
+cd
+mkdir -p /usr/lib/openvpn/
+cp /usr/lib/x86_64-linux-gnu/openvpn/plugins/openvpn-plugin-auth-pam.so /usr/lib/openvpn/openvpn-plugin-auth-pam.so
+
+# nano /etc/default/openvpn
+sed -i 's/#AUTOSTART="all"/AUTOSTART="all"/g' /etc/default/openvpn
+
+# restart openvpn dan cek status openvpn
+systemctl enable --now openvpn-server@server-tcp-1194
+/etc/init.d/openvpn restart
+/etc/init.d/openvpn status
+
+# aktifkan ip4 forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+
+# Buat config client TCP 1194
+cat > /etc/openvpn/TCP.ovpn <<-END
+client
+verb 3
+dev tun
+proto tcp
+resolv-retry infinite
+route-method exe
+nobind
+persist-key
+persist-tun
+ping 5
+ping-restart 30
+comp-lzo
+auth-user-pass
+remote xxxxxxxxx 1194
+http-proxy xxxxxxxxx 8080
+END
+
+sed -i $MYIP2 /etc/openvpn/TCP.ovpn;
+
+cd
+# pada tulisan xxx ganti dengan alamat ip address VPS anda 
+/etc/init.d/openvpn restart
+
+# masukkan certificatenya ke dalam config client TCP 1194
+echo '<ca>' >> /etc/openvpn/TCP.ovpn
+cat /etc/openvpn/server/ca.crt >> /etc/openvpn/TCP.ovpn
+echo '</ca>' >> /etc/openvpn/TCP.ovpn
+
+# Copy config OpenVPN client ke home directory root agar mudah didownload ( TCP 1194 )
+cp /etc/openvpn/TCP.ovpn /home/vps/public_html/TCP.ovpn
+
+#firewall untuk memperbolehkan akses SSL dan akses jalur TCP
+
+iptables -t nat -I POSTROUTING -s 10.6.0.0/24 -o $ANU -j MASQUERADE
+iptables -t nat -I POSTROUTING -s 10.7.0.0/24 -o $ANU -j MASQUERADE
+iptables-save > /etc/iptables.up.rules
+chmod +x /etc/iptables.up.rules
+
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save
+netfilter-persistent reload
+
+# Restart service openvpn
+systemctl enable openvpn
+systemctl start openvpn
+/etc/init.d/openvpn restart
+
+# Delete script
+history -c
+rm -f /root/vpn.sh
+
+
